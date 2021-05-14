3fa4a9f1e9367019575356d4591a3b311ea0d249
DarkKnight <1069306849@qq.com>
2021-05-14 16:12:19 +0800

    2001动态相册一

--- 2001html/mywebsite/projects/fan/js/index.js
+++ 2001html/mywebsite/projects/fan/js/index.js
@@ -3,7 +3,11 @@ console.log('in index.js======>');
 let imgFan = document.getElementById('imgFan');
 // 找到页面上对应css选择器的一组元素
 let controls = document.querySelectorAll('.control > span');
-console.log('风扇元素：', imgFan, controls);
+// 音频元素
+let myaudio = document.getElementById('myaudio');
+// 开启音频循环播放模式
+myaudio.loop = true;
+console.log('风扇元素：', imgFan, controls, myaudio);
 
 // 记录是否开启
 let isOn = false;
@@ -21,6 +25,10 @@ controls[0].addEventListener('click', function () {
   clearActive();
   // 激活自己
   controls[0].classList.add('active');
+  // 关闭音频播放
+  myaudio.pause();
+  // 重置播放进度到初始位置
+  myaudio.currentTime = 0;
 });
 
 controls[1].addEventListener('click', function () {
@@ -33,24 +41,36 @@ controls[1].addEventListener('click', function () {
   imgFan.classList.remove('fan0', 'fan1', 'fan2', 'fan3');
   // 一档动画
   imgFan.classList.add('fan1');
+  // 开启音频播放
+  myaudio.play();
 });
 
 controls[2].addEventListener('click', function () {
+  clearActive();
+  // 激活自己
+  controls[2].classList.add('active');
   // 开启状态
   isOn = true;
   // 清除掉所有的动画
   imgFan.classList.remove('fan0', 'fan1', 'fan2', 'fan3');
   // 二档动画
   imgFan.classList.add('fan2');
+  // 开启音频播放
+  myaudio.play();
 });
 
 controls[3].addEventListener('click', function () {
+  clearActive();
+  // 激活自己
+  controls[3].classList.add('active');
   // 开启状态
   isOn = true;
   // 清除掉所有的动画
   imgFan.classList.remove('fan0', 'fan1', 'fan2', 'fan3');
   // 三档动画
   imgFan.classList.add('fan3');
+  // 开启音频播放
+  myaudio.play();
 });
 
 function clearActive() {
