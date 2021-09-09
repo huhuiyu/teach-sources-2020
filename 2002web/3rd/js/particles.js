a74c1b6416a64a06dbfad17bbb5e81815f773b7b
DarkKnight <1069306849@qq.com>
2021-09-09 11:39:44 +0800

    2002，2004班第一课

---
+++ 2002web/3rd/js/particles.js
@@ -0,0 +1,5 @@
+// 第一个参数是页面元素的id
+// 第二个参数是配置文件的地址名称
+particlesJS.load(
+  'divParticles'
+,'particles.json');
---
+++ 2004web/3rd/js/particles.js
@@ -0,0 +1,3 @@
+// 第一个参数是页面元素的id，该元素会变成粒子效果
+// 第二个参数是的配置文件的链接地址
+particlesJS.load('divParticles','particles.json');
