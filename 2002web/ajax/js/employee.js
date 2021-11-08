296ca6210d01959b7f1456925fb773a0f4e48305
DarkKnight <1069306849@qq.com>
2021-11-08 12:42:18 +0800

    2002,2004班员工管理二阶段

--- 2002web/ajax/js/employee.js
+++ 2002web/ajax/js/employee.js
@@ -10,8 +10,34 @@ let page = {
 let deptList = [];
 let list = [];
 
+// 查询的表单
+let selQDept = document.getElementById('selQDept');
+let txtQName = document.getElementById('txtQName');
+let txtQPhone = document.getElementById('txtQPhone');
+let btnQuery = document.getElementById('btnQuery');
+let btnReset = document.getElementById('btnReset');
+
+btnQuery.addEventListener('click', query);
+
+btnReset.addEventListener('click', function () {
+  // 查询条件重置
+  queryInfo = {};
+  page.pageNumber = 1;
+  // 表单重置
+  selQDept.value = -1;
+  txtQName.value = '';
+  txtQPhone.value = '';
+  // 数据重查
+  query();
+});
+
 // 查询数据
 function query() {
+  // 处理参数
+  queryInfo.deptId = selQDept.value;
+  queryInfo.employeeName = txtQName.value;
+  queryInfo.phone = txtQPhone.value;
+
   ajaxRequest(
     '/manange/employee/query',
     {
@@ -31,6 +57,10 @@ function query() {
       page = data.resultData.page;
       // 显示信息表格
       showEmpInfo();
+      // 显示分页信息
+      showPageInfo();
+      // 初始化部门列表
+      showDept();
     }
   );
 }
@@ -46,7 +76,7 @@ function showEmpInfo() {
     let tr = document.createElement('tr');
     // 所属部门
     let td = document.createElement('td');
-    td.append(emp.deptId);
+    td.append(getDeptName(emp.deptId));
     tr.append(td);
     // 姓名
     td = document.createElement('td');
@@ -64,6 +94,108 @@ function showEmpInfo() {
     tbData.append(tr);
   }
 }
+
+// 显示部门列表
+function showDept() {
+  // 查询的部门
+  selQDept.innerHTML = '';
+  // 伪造一个请选择的部门
+  let mop = document.createElement('option');
+  mop.append('请选择部门');
+  //值是不存在的，方便服务器端判定
+  mop.setAttribute('value', -1);
+  selQDept.append(mop);
+
+  for (let i = 0; i < deptList.length; i++) {
+    let dept = deptList[i];
+    let op = document.createElement('option');
+    op.append(dept.deptName);
+    op.setAttribute('value', dept.deptId);
+    selQDept.append(op);
+  }
+  // 如果有部门查询条件就要选中
+  if (queryInfo.deptId) {
+    selQDept.value = queryInfo.deptId;
+  }
+
+  // 添加的部门
+  selDept.innerHTML = '';
+
+  for (let i = 0; i < deptList.length; i++) {
+    let dept = deptList[i];
+    let op = document.createElement('option');
+    op.append(dept.deptName);
+    op.setAttribute('value', dept.deptId);
+    selDept.append(op);
+  }
+}
+
+// 换算部门的编号为部门名称
+function getDeptName(id) {
+  for (let i = 0; i < deptList.length; i++) {
+    let dept = deptList[i];
+    // 比对传入的id是否匹配
+    if (id == dept.deptId) {
+      return dept.deptName;
+    }
+  }
+  return '部门未知';
+}
+
+// 分页的部分
+let spPre = document.getElementById('spPre');
+let spPage = document.getElementById('spPage');
+let spNext = document.getElementById('spNext');
+
+function showPageInfo() {
+  spPage.innerHTML =
+    '当前页/总页数/记录数：' +
+    page.pageNumber +
+    '/' +
+    page.pageCount +
+    '/' +
+    page.total;
+}
+
+// 下一页功能
+spNext.addEventListener('click', function () {
+  page.pageNumber = page.pageNumber + 1;
+  // 不要超过总页数
+  if (page.pageNumber > page.pageCount) {
+    page.pageNumber = page.pageCount;
+    return;
+  }
+  // 更改页码要重新查询
+  query();
+});
+
+// 上一页
+spPre.addEventListener('click', function () {
+  page.pageNumber = page.pageNumber - 1;
+  if (page.pageNumber < 1) {
+    page.pageNumber = 1;
+    return;
+  }
+  query();
+});
+
+// 添加相关
+let btnAdd = document.getElementById('btnAdd');
+let divAddDialog = document.getElementById('divAddDialog');
+let txtName = document.getElementById('txtName');
+let txtPhone = document.getElementById('txtPhone');
+let btnAddInfo = document.getElementById('btnAddInfo');
+let btnCloseAdd = document.getElementById('btnCloseAdd');
+let selDept = document.getElementById('selDept');
+
+btnAdd.addEventListener('click', function () {
+  divAddDialog.style.display = 'flex';
+});
+
+btnCloseAdd.addEventListener('click', function () {
+  divAddDialog.style.display = 'none';
+  query();
+});
 
 // 两个星期之内完成，上课会展示
 // 仿造上课的部门员工管理，完成班级和学生的信息管理
--- 2004web/ajax/js/employee.js
+++ 2004web/ajax/js/employee.js
@@ -5,10 +5,34 @@ let list = []; // 员工列表
 let page = { pageNumber: 1, pageSize: 5 }; // 分页信息
 let queryInfo = {}; //查询条件
 
+let selQDept = document.getElementById('selQDept');
+let txtQName = document.getElementById('txtQName');
+let txtQPhone = document.getElementById('txtQPhone');
+let btnQuery = document.getElementById('btnQuery');
+let btnReset = document.getElementById('btnReset');
+
+btnQuery.addEventListener('click', query);
+
+btnReset.addEventListener('click', function () {
+  // 查询表单重置
+  selQDept.value = -1;
+  txtQName.value = '';
+  txtQPhone.value = '';
+  // 查询条件重置
+  queryInfo = {};
+  // 重新查询
+  query();
+});
+
 // 查询员工信息
 function query() {
   // 显示等待对话框
   waitDialog.style.display = 'flex';
+  // 查询条件
+  queryInfo.deptId = selQDept.value;
+  queryInfo.employeeName = txtQName.value;
+  queryInfo.phone = txtQPhone.value;
+
   ajaxRequest(
     '/manange/employee/query',
     {
@@ -24,6 +48,8 @@ function query() {
         page = data.resultData.page;
         // 显示列表
         showEmpInfo();
+        // 显示分页
+        showPageInfo();
         // 显示部门下拉列表
         showDeptList();
       } else {
@@ -58,8 +84,59 @@ function showEmpInfo() {
     td = document.createElement('td');
     td.append(formatTimestamp(emp.lastupdate));
     tr.append(td);
+    // 功能部分
+    td = document.createElement('td');
+    // 删除信息
+    let btnDel = document.createElement('button');
+    td.append(btnDel);
+    btnDel.append('删除');
+    btnDel.addEventListener('click', function () {
+      delInfo(emp);
+    });
+    // 修改信息
+    let btnModify = document.createElement('button');
+    td.append(btnModify);
+    btnModify.append('修改');
+    btnModify.addEventListener('click', function () {
+      showModifyInfo(emp);
+    });
+
+    tr.append(td);
 
     tbData.append(tr);
+  }
+}
+
+// 修改的部分
+let divModifyDialog = document.getElementById('divModifyDialog');
+let selMDept = document.getElementById('selMDept');
+let txtMName = document.getElementById('txtMName');
+let txtMPhone = document.getElementById('txtMPhone');
+let btnSaveEmp = document.getElementById('btnSaveEmp');
+let btnCloseModify = document.getElementById('btnCloseModify');
+let modifyInfo = null;
+
+function showModifyInfo(info) {
+  modifyInfo = info;
+  selMDept.value = modifyInfo.deptId;
+  txtMName.value = modifyInfo.employeeName;
+  txtMPhone.value = modifyInfo.phone;
+  divModifyDialog.style.display = 'flex';
+}
+
+// 删除的部分
+function delInfo(info) {
+  if (confirm('是否删除：' + info.employeeName)) {
+    ajaxRequest(
+      '/manange/employee/delete',
+      {
+        'tbEmployee.employeeId': info.employeeId
+      },
+      function (data) {
+        alert(data.message);
+        query();
+      }
+    );
   }
 }
 
@@ -104,6 +181,7 @@ btnAddEmp.addEventListener('click', function () {
 
 // 显示部门列表
 function showDeptList() {
+  // 添加的部门列表
   selDept.innerHTML = '';
   for (let i = 0; i < deptList.length; i++) {
     let dept = deptList[i];
@@ -112,6 +190,37 @@ function showDeptList() {
     op.setAttribute('value', dept.deptId);
     op.append(dept.deptName);
     selDept.append(op);
+  }
+
+  // 修改的部门列表
+  selMDept.innerHTML = '';
+  for (let i = 0; i < deptList.length; i++) {
+    let dept = deptList[i];
+    let op = document.createElement('option');
+    op.setAttribute('value', dept.deptId);
+    op.append(dept.deptName);
+    selMDept.append(op);
+  }
+
+  // 查询的部门列表
+  selQDept.innerHTML = '';
+  // 查询允许不指定部门，所以需要伪造一个不存在的部门值给服务器
+  let qdept = document.createElement('option');
+  qdept.setAttribute('value', -1);
+  qdept.append('===请选择部门===');
+  selQDept.append(qdept);
+
+  for (let i = 0; i < deptList.length; i++) {
+    let dept = deptList[i];
+    let op = document.createElement('option');
+    // 值是编号！！！
+    op.setAttribute('value', dept.deptId);
+    op.append(dept.deptName);
+    selQDept.append(op);
+  }
+  // 如果存在查询条件，需要指定值为查询条件的值
+  if (queryInfo.deptId) {
+    selQDept.value = queryInfo.deptId;
   }
 }
 
@@ -128,3 +237,42 @@ function getDeptName(deptId) {
 
   return '部门不存在';
 }
+
+// 分页的部分
+let spPre = document.getElementById('spPre');
+let spPage = document.getElementById('spPage');
+let spNext = document.getElementById('spNext');
+
+function showPageInfo() {
+  // 显示分页信息
+  spPage.innerHTML =
+    '当前页/总页数/记录数：' +
+    page.pageNumber +
+    '/' +
+    page.pageCount +
+    '/' +
+    page.total;
+}
+
+// 下一页
+spNext.addEventListener('click', function () {
+  page.pageNumber++;
+  // 不能超过总页数
+  if (page.pageNumber > page.pageCount) {
+    page.pageNumber = page.pageCount;
+    return;
+  }
+  // 查询新页码的数据
+  query();
+});
+
+// 上一页
+spPre.addEventListener('click', function () {
+  page.pageNumber--;
+  // 不能小于1
+  if (page.pageNumber < 1) {
+    page.pageNumber = 1;
+    return;
+  }
+  query();
+});
