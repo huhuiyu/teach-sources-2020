8315cbbf411c8f93ddf5476eb3096d02eb744e20
huhuiyu <1069306849@qq.com>
2021-11-25 11:39:52 +0800

    2002,2004 bs5第二课

---
+++ 2004web/bootstrap/js/bs03.js
@@ -0,0 +1,16 @@
+let btnClose = document.getElementById('btnClose');
+
+// 对已有功能按钮添加事件不影响原有动作
+btnClose.addEventListener('click', function () {
+  console.log('点击了打开对话框按钮');
+});
+
+let divDialog = document.getElementById('div-dialog');
+// bs5对话框自定义事件
+divDialog.addEventListener('shown.bs.modal', function () {
+  console.log('对话框已经显示完成');
+  document.querySelector('.modal-body input:first-of-type').focus();
+});
+divDialog.addEventListener('hidden.bs.modal', function () {
+  console.log('对话框已经关闭完成');
+});
