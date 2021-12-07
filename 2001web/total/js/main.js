021d022fc00143dd2c8014846510fc48ad0b5063
huhuiyu <1069306849@qq.com>
2021-12-07 16:12:37 +0800

    2001班用户首页

--- 2001web/total/js/main.js
+++ 2001web/total/js/main.js
@@ -8,7 +8,8 @@ function getUserInfo() {
     if (data.success) {
       user = data.resultData.loginInfo;
       userattr = user.userInfo;
-      console.log('用户信息：', user);
+      console.log('用户信息：', user, userattr);
+      showUser();
     } else {
       alert('获取用户信息失败！');
       location = 'login.html';
@@ -18,3 +19,60 @@ function getUserInfo() {
 
 getUserInfo();
 // 获取用户信息结束 =============================
+
+// 页面元素 ===================================
+let spUser = document.getElementById('spUser');
+
+let btnSave = document.getElementById('btnSave');
+let txtWechat = document.getElementById('txtWechat');
+let txtQq = document.getElementById('txtQq');
+let txtPhone = document.getElementById('txtPhone');
+let txtInfo = document.getElementById('txtInfo');
+let txtImg = document.getElementById('txtImg');
+let txtEmail = document.getElementById('txtEmail');
+let selSex = document.getElementById('selSex');
+let divUDialog = document.getElementById('divUDialog');
+
+// 页面元素结束 ===================================
+
+// 显示用户基本信息
+function showUser() {
+  spUser.innerHTML = user.username + '(' + user.nickname + ')';
+}
+
+// 基本信息对话框显示的时候需要填写默认数据
+divUDialog.addEventListener('shown.bs.modal', function () {
+  selSex.value = userattr.sex;
+  txtEmail.value = userattr.email;
+  txtImg.value = userattr.img;
+  txtInfo.value = userattr.info;
+  txtPhone.value = userattr.phone;
+  txtQq.value = userattr.qq;
+  txtWechat.value = userattr.wechat;
+});
+
+// 关闭时要重新查询用户信息
+divUDialog.addEventListener('hidden.bs.modal', function () {
+  getUserInfo();
+});
+
+// 保存用户附加信息
+btnSave.addEventListener('click', function () {
+  ajax(
+    '/user/modifyUserInfo',
+    {
+      tbUserInfo: {
+        email: txtEmail.value,
+        img: txtImg.value,
+        info: txtInfo.value,
+        phone: txtPhone.value,
+        qq: txtQq.value,
+        sex: selSex.value,
+        wechat: txtWechat.value
+      }
+    },
+    function (data) {
+      alert(data.message);
+    }
+  );
+});
