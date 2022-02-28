3b91935e21dc87ea42ffd94398e2cf1a6c2c21b5
huhuiyu <1069306849@qq.com>
2022-02-28 16:12:22 +0800

    2004班vue

---
+++ 2004vue/js/vue01.js
@@ -0,0 +1,25 @@
+// 初始化一个vue实例
+new Vue({
+  // el表示实例绑定的页面元素，可以是选择器表达式
+  // #app表示和页面id="app"的元素绑定
+  el: '#app',
+  // data函数表示页面需要使用的数据模型(json格式)
+  // data也可以直接是一个json对象（函数在组件里面为必须）
+  // data中定义的数据可以直接在页面中使用！！！
+  data() {
+    return {
+      info: '黑暗骑士的vue项目',
+      // json的层次可以是任意的！！！！
+      userinfo: {
+        username: 'admin',
+        password: ''
+      }
+    };
+  }
+});
+
+// vue是mvvm框架
+// v是view（视图），对应的就是页面模板内容
+// m是Model（数据模型），对应到data定义数据
+// vm就是视图模型
+// vm可以将视图和模型中的变化同步给另外一方！！！
