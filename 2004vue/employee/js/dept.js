804216432f283a4c400452634aebd1c5f24fc374
huhuiyu <1069306849@qq.com>
2022-03-21 16:22:35 +0800

    2004版本vue部门管理完成

--- 2004vue/employee/js/dept.js
+++ 2004vue/employee/js/dept.js
@@ -1,3 +1,4 @@
+
 new Vue({
   el: '#app',
   data() {
@@ -15,9 +16,82 @@ new Vue({
         pageNumber: 1,
         pageSize: 5,
       },
+      // 添加的信息
+      addInfo: {
+        deptInfo: '',
+        deptName: '',
+      },
+      // 修改的信息
+      modifyInfo: {},
+      // 修改信息是否可见
+      visible: false,
     };
   },
   methods: {
+    getTime(time) {
+      console.log('要处理的时间信息', time);
+      // 转换时间戳为指定格式的字符
+      let date = new Date();
+      date.setTime(time);
+      return (
+        date.getFullYear() +
+        '-' +
+        (date.getMonth() + 1) +
+        '-' +
+        date.getDate() +
+        ' ' +
+        date.getHours() +
+        ':' +
+        date.getMinutes() +
+        ':' +
+        date.getSeconds()
+      );
+    },
+    del(info) {
+      let app = this;
+      if (confirm('是否删除部门：' + info.deptName)) {
+        app.loading = true;
+        ajax(
+          '/manage/dept/delete',
+          {
+            deptId: info.deptId,
+          },
+          function (data) {
+            app.loading = false;
+            alert(data.message);
+            app.query();
+          }
+        );
+      }
+    },
+    modify() {
+      // 修改信息
+      let app = this;
+      app.loading = true;
+      ajax('/manage/dept/update', app.modifyInfo, function (data) {
+        app.loading = false;
+        alert(data.message);
+      });
+    },
+    showModify(info) {
+      // vue是双向绑定，所以需要一个页面数据的副本进行修改
+      this.modifyInfo = JSON.parse(JSON.stringify(info));
+      // 删除修改不能提交的字段lastupdate
+      delete this.modifyInfo.lastupdate;
+      this.visible = true;
+    },
+    add() {
+      let app = this;
+      app.loading = true;
+      ajax('/manage/dept/add', app.addInfo, function (data) {
+        app.loading = false;
+        alert(data.message);
+        if (data.success) {
+          app.addInfo = {};
+          app.query();
+        }
+      });
+    },
     toPage(pageNumber) {
       // 分页合法性校验
       if (pageNumber <= 0 || pageNumber > this.page.pageCount) {
---
+++ 2004vue/js/myfilters.js
@@ -0,0 +1,5 @@
+// 全局过滤器定义
+// 格式化时间的过滤器
+Vue.filter('formatDate', function (value) {
+  return 'filter：' + value;
+});
