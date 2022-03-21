ae9a01c4c6dc6f263763353aa7c30de386598d6e
huhuiyu <1069306849@qq.com>
2022-03-21 16:22:56 +0800

    2001版本部门管理完成

--- 2001vue/employee/js/dept.js
+++ 2001vue/employee/js/dept.js
@@ -17,9 +17,73 @@ new Vue({
       },
       // 加载的控制
       loading: false,
+      // 添加的信息
+      addInfo: {
+        deptName: '',
+        deptInfo: '',
+      },
+      // 修改的信息
+      modifyInfo: {},
+      // 修改的表单是否可见
+      visible: false,
     };
   },
   methods: {
+    showModify(info) {
+      let app = this;
+      app.visible = true;
+      // vue特色是vm同步，所以修改的值要是副本
+      app.modifyInfo = JSON.parse(JSON.stringify(info));
+      // 服务器不需要lastupdate字段
+      delete app.modifyInfo.lastupdate;
+    },
+    modify() {
+      let app = this;
+      ajax('/manage/dept/update', app.modifyInfo, function (data) {
+        alert(data.message);
+        app.query();
+      });
+    },
+    del(info) {
+      console.log('要删除的信息：', info);
+      let app = this;
+      if (confirm('是否删除：' + info.deptName)) {
+        app.loading = true;
+        ajax(
+          '/manage/dept/delete',
+          {
+            deptId: info.deptId,
+          },
+          function (data) {
+            app.loading = false;
+            alert(data.message);
+            app.query();
+          }
+        );
+      }
+    },
+    add() {
+      // 添加部门的方法
+      this.loading = true;
+      let app = this;
+      ajax('/manage/dept/add', app.addInfo, function (data) {
+        app.loading = false;
+        alert(data.message);
+        if (data.success) {
+          // 成功就重置添加并查询
+          app.addInfo = {
+            deptName: '',
+            deptInfo: '',
+          };
+          app.query();
+        }
+      });
+    },
+    requery() {
+      // 条件查询
+      this.page.pageNumber = 1; //回到第一页
+      this.query();
+    },
     toPage(pageNumber) {
       // 有效性判断
       if (pageNumber < 1 || pageNumber > this.page.pageCount) {
