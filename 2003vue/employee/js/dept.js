189698b1089aa5a73422b9e306ca99b18918cbb2
huhuiyu <1069306849@qq.com>
2022-03-23 18:07:21 +0800

    2003班部门管理完成

--- 2003vue/employee/js/dept.js
+++ 2003vue/employee/js/dept.js
@@ -15,9 +15,67 @@ new Vue({
         pageNumber: 1,
         pageSize: 5,
       },
+      // 添加信息对象
+      addInfo: {
+        deptInfo: '',
+        deptName: '',
+      },
+      // 修改信息对象
+      modifyInfo: {},
+      // 修改信息是否出现
+      visible: false,
+      // 加载数据的控制
+      loading: false,
     };
   },
   methods: {
+    del(info) {
+      // 删除信息
+      let app = this;
+      if (confirm('是否删除部门:' + info.deptName)) {
+        app.loading = true;
+        ajax(
+          '/manage/dept/delete',
+          {
+            deptId: info.deptId,
+          },
+          function (data) {
+            app.loading = false;
+            alert(data.message);
+            app.query();
+          }
+        );
+      }
+    },
+    modify() {
+      // 修改信息
+      let app = this;
+      app.loading = true;
+      ajax('/manage/dept/update', app.modifyInfo, function (data) {
+        app.loading = false;
+        alert(data.message);
+        if (data.success) {
+          app.query();
+        }
+      });
+    },
+    showModify(info) {
+      // 显示修改信息，vue的变量会和页面绑定，所以修改应该是副本
+      this.modifyInfo = JSON.parse(JSON.stringify(info));
+      this.visible = true;
+    },
+    add() {
+      // 添加部门
+      let app = this;
+      app.loading = true;
+      ajax('/manage/dept/add', this.addInfo, function (data) {
+        app.loading = false;
+        alert(data.message);
+        if (data.success) {
+          app.addInfo = { deptInfo: '', deptName: '' };
+        }
+      });
+    },
     toPage(pn) {
       // 分页有效性判断
       if (pn < 1 || pn > this.page.pageCount) {
@@ -32,9 +90,10 @@ new Vue({
       // 处理分页的信息
       app.queryInfo.pageNumber = app.page.pageNumber;
       app.queryInfo.pageSize = app.page.pageSize;
-
+      app.loading = true;
       // 查询部门信息
       ajax('/manage/dept/queryAll', app.queryInfo, function (data) {
+        app.loading = false;
         if (!data.success) {
           alert(data.message);
           return;
