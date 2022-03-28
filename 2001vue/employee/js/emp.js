e0ef9401244a52bdbc0e18355b03b9c2d917cb7c
huhuiyu <1069306849@qq.com>
2022-03-28 09:51:55 +0800

    2001班员工查询功能完成

--- 2001vue/employee/js/emp.js
+++ 2001vue/employee/js/emp.js
@@ -26,9 +26,78 @@ new Vue({
         { value: '4', text: '按照部门名称降序' },
         { value: '5', text: '分部门按照姓名排序' },
       ],
+      // 部门查询的部分
+      dept: {
+        page: { pageSize: 5 },
+        list: [],
+        visible: false,
+        selected: {},
+      },
+      // 添加的部分
+      addInfo: {
+        deptId: -1,
+        employeeName: '',
+        phone: '',
+      },
     };
   },
   methods: {
+    // 添加部门
+    add() {
+      ajax('/manage/employee/add', app.addInfo, function (data) {
+        alert(data.message);
+        if (data.success) {
+          app.resetAdd();
+          app.query();
+        }
+      });
+    },
+    // 重置添加
+    resetAdd() {
+      app.addInfo = {
+        deptId: -1,
+        employeeName: '',
+        phone: '',
+      };
+    },
+    // 部门分页
+    toDeptPage(pn) {
+      if (pn < 1 || pn > app.dept.page.pageCount) {
+        return;
+      }
+      app.dept.page.pageNumber = pn;
+      app.queryDept();
+    },
+    // 重置查询
+    resetQuery() {
+      app.queryInfo = {
+        deptId: -1,
+        employeeName: '',
+        orderBy: '2',
+        phone: '',
+      };
+      app.query();
+    },
+    // 选中部门
+    selectDept(info) {
+      // 记录选中值
+      app.dept.selected = info;
+      // 修改查询条件值
+      app.queryInfo.deptId = info.deptId;
+      app.dept.visible = false;
+    },
+    // 查询部门的部分
+    queryDept() {
+      ajax('/manage/dept/queryAll', app.dept.page, function (data) {
+        if (data.success) {
+          app.dept.visible = true;
+          app.dept.page = data.page;
+          app.dept.list = data.list;
+        } else {
+          alert(data.message);
+        }
+      });
+    },
     query() {
       // 需要处理分页信息
       copyJsonInfo(app.page, app.queryInfo);
