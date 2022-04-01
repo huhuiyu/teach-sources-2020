e0323781f0978c73ce48229cea52d505b9d75535
huhuiyu <1069306849@qq.com>
2022-04-01 09:51:17 +0800

    2003版本员工管理完成

--- 2003vue/employee/js/emp.js
+++ 2003vue/employee/js/emp.js
@@ -34,6 +34,7 @@ new Vue({
         visible: false,
         // 记录选中的项
         selected: {},
+        selectedAdd: {},
         // 选择部门的模式
         mode: '',
       },
@@ -43,9 +44,53 @@ new Vue({
         employeeName: '',
         phone: '',
       },
+      // 修改的信息
+      modiVisible: false,
+      modiInfo: {},
     };
   },
   methods: {
+    del(info) {
+      if (confirm('是否删除：' + info.employeeName)) {
+        ajax(
+          '/manage/employee/delete',
+          {
+            employeeId: info.employeeId,
+          },
+          function (data) {
+            alert(data.message);
+            app.query();
+          }
+        );
+      }
+    },
+    modify() {
+      // 部门信息不需要上传服务器
+      let info = JSON.parse(JSON.stringify(app.modiInfo));
+      // delete json对象.字段名 是删除json对象的指定字段
+      delete info.dept;
+      // 保存
+      ajax('/manage/employee/update', info, function (data) {
+        alert(data.message);
+        app.query();
+      });
+    },
+    showModi(info) {
+      app.modiInfo = JSON.parse(JSON.stringify(info));
+      app.modiVisible = true;
+    },
+    add() {
+      ajax('/manage/employee/add', app.addInfo, function (data) {
+        alert(data.message);
+        if (data.success) {
+          app.addInfo = {
+            employeeName: '',
+            phone: '',
+          };
+        }
+        app.query();
+      });
+    },
     toDeptPage(pn) {
       // 分页有效性判断
       if (pn < 1 || pn > app.deptInfo.page.pageCount) {
@@ -55,20 +100,24 @@ new Vue({
       app.deptInfo.page.pageNumber = pn;
       app.queryDept();
     },
-
     // 选中部门的部分
     selectDept(info) {
-      // 记住选中的值
-      app.deptInfo.selected = info;
-
       if ('add' == app.deptInfo.mode) {
         // 给添加部门赋值
         app.addInfo.deptId = info.deptId;
+        // 记住选中的值
+        app.deptInfo.selectedAdd = info;
       } else if ('query' == app.deptInfo.mode) {
         // 给查询条件赋值
         app.queryInfo.deptId = info.deptId;
+        // 记住选中的值
+        app.deptInfo.selected = info;
+      } else if ('modi' == app.deptInfo.mode) {
+        // 给修改条件赋值
+        app.modiInfo.deptId = info.deptId;
+        // 记住选中的值
+        app.modiInfo.dept = info;
       }
-
       app.deptInfo.visible = false;
     },
     // 查询部门的部分
