c01afac0e4f364ca3112647e286f2eb6475dbd52
huhuiyu <1069306849@qq.com>
2022-05-09 09:44:13 +0800

    2001班vue模板项目

---
+++ 2001-vuecli-projects/vue-router-vuex/.prettierrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  // 去掉分号
+  semi: false,
+  // 字符串单引号
+  singleQuote: true,
+  // 配置结束标记不要折断
+  htmlWhitespaceSensitivity: 'strict',
+  printWidth: 400,
+  proseWrap: 'preserve',
+  // json自动补逗号
+  trailingComma: 'es5',
+}
---
+++ 2001-vuecli-projects/vue-router-vuex/src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<template>
+  <div> 网站建设中。。。 </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeView',
+}
+</script>
