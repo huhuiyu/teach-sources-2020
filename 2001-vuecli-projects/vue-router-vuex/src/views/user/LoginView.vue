e18935f47f97c990c419375656c439e5e879fdb1
huhuiyu <1069306849@qq.com>
2022-05-19 17:52:21 +0800

    2001班登录完成

--- 2001-vuecli-projects/vue-router-vuex/src/views/user/LoginView.vue
+++ 2001-vuecli-projects/vue-router-vuex/src/views/user/LoginView.vue
@@ -1,21 +1,90 @@
 <template>
-  <div>
-    <div>{{ title }}</div>
+  <div class="main">
+    <div>
+      <el-card>
+        <div slot="header">
+          <i class="iconfont">&#xe64a;</i>
+          {{ title }}
+        </div>
+
+        <div>
+          <el-form :model="user" v-loading="loading">
+            <el-form-item>
+              <el-input v-model="user.username" placeholder="登录名">
+                <i slot="prefix" class="el-input__icon iconfont">&#xe64a;</i>
+              </el-input>
+            </el-form-item>
+
+            <el-form-item>
+              <el-input type="password" show-password v-model="user.password" placeholder="密码">
+                <i slot="prefix" class="el-input__icon iconfont">&#xe620;</i>
+              </el-input>
+            </el-form-item>
+
+            <el-form-item class="tc">
+              <el-button @click="login" type="primary"><i class="iconfont">&#xe600;</i> 登录 </el-button>
+              <el-button @click="reset" type="danger"><i class="iconfont">&#xe648;</i> 重填</el-button>
+            </el-form-item>
+          </el-form>
+        </div>
+      </el-card>
+    </div>
   </div>
 </template>
 <script>
+import tools from '../../js/tools'
 let app
 export default {
   name: 'UserLoginView',
   data() {
     return {
       title: '用户登录',
+      user: {
+        username: '',
+        password: '',
+      },
+      loading: false,
     }
   },
-  methods: {},
+  methods: {
+    login() {
+      app.loading = true
+      app.user.password = tools.md5(app.user.password)
+      tools.ajax('/user/auth/login', app.user, (data) => {
+        app.loading = false
+        app.user.password = ''
+        if (data.success) {
+          // 成功就跳转到用户首页
+          app.$router.push('/user/main')
+          return
+        }
+        // 弹出错误提示
+        app.$message({
+          message: data.message,
+          type: 'error',
+        })
+        // app.$alert(data.message, '错误')
+      })
+    },
+    reset() {
+      app.user = {
+        username: '',
+        password: '',
+      }
+    },
+  },
   created() {
     app = this
     console.log(app.title)
   },
 }
 </script>
+<style scoped>
+.main {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  width: 100vw;
+}
+</style>
--- 2001-vuecli-projects/vue-router-vuex/src/views/user/MainView.vue
+++ 2001-vuecli-projects/vue-router-vuex/src/views/user/MainView.vue
@@ -1,21 +1,83 @@
 <template>
   <div>
-    <div>{{ title }}</div>
+    <div class="top-box">
+      <div>{{ title }}</div>
+      <div>
+        <el-dropdown split-button type="primary">
+          <i class="iconfont">&#xe64a;</i>
+          <el-dropdown-menu slot="dropdown">
+            <el-dropdown-item>
+              <i class="iconfont">&#xe64a;</i>
+              {{ tbUser.username }}
+              ({{ tbUser.nickname }})
+            </el-dropdown-item>
+
+            <el-dropdown-item>
+              <i class="iconfont">&#xe660;</i>
+              {{ tbUser.accessKey }}
+            </el-dropdown-item>
+
+            <el-dropdown-item divided>
+              <i class="iconfont">&#xe651;</i>
+              安全退出
+            </el-dropdown-item>
+          </el-dropdown-menu>
+        </el-dropdown>
+      </div>
+    </div>
+
+    <div>
+      {{ tbUser }}
+      <hr />
+      {{ tbUserInfo }}
+      <hr />
+      {{ userOtherInfo }}
+    </div>
   </div>
 </template>
 <script>
+import tools from '../../js/tools'
 let app
 export default {
   name: 'UserMainView',
   data() {
     return {
       title: '用户首页',
+      tbUser: {},
+      tbUserInfo: {},
+      userOtherInfo: {},
     }
   },
-  methods: {},
+  methods: {
+    queryUser() {
+      // 获取登录用户信息
+      tools.ajax('/user/auth/getUserInfo', {}, (data) => {
+        if (data.success) {
+          app.tbUser = data.tbUser
+          app.tbUserInfo = data.tbUserInfo
+          app.userOtherInfo = data.userOtherInfo
+        } else {
+          app.$alert(data.message, '错误')
+        }
+      })
+    },
+  },
   created() {
     app = this
     console.log(app.title)
+    app.queryUser()
   },
 }
 </script>
+<style scoped>
+.top-box {
+  display: flex;
+  justify-content: space-between;
+  padding: 0.5rem;
+  align-items: center;
+}
+/* 每一个饿了么组件都有一个对应名称class供用户调整样式 */
+.el-dropdown .iconfont {
+  font-size: 0.9em;
+}
+</style>
