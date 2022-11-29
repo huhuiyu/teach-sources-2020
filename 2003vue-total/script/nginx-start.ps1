6d9b1c78e281ba79faed62e9a622a6e5a0a9254d
huhuiyu <1069306849@qq.com>
2022-11-29 11:29:17 +0800

    2003班nginx脚本

---
+++ 2003vue-total/script/nginx-start.ps1
@@ -0,0 +1,4 @@
+# 切换到nginx所在文件夹
+cd D:\develop\nginx-1.22.1
+# 启动nginx
+.\nginx -c .\conf\nginx-2003.conf
---
+++ 2003vue-total/script/nginx-stop.ps1
@@ -0,0 +1,4 @@
+# 切换到nginx所在文件夹
+cd D:\develop\nginx-1.22.1
+# 停止nginx
+.\nginx -s stop -c .\conf\nginx-2003.conf
