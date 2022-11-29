30191cd0d00f6f3607ef299dc36693d5eae593bb
huhuiyu <1069306849@qq.com>
2022-11-29 18:12:18 +0800

    nginx相关脚本

---
+++ 2001vue-total/script/restart.ps1
@@ -0,0 +1,4 @@
+# 切换到nginx所在文件夹
+cd D:\develop\nginx-1.22.1
+# 重启nginx（更新配置信息）
+.\nginx -s reload
--- 2001vue-total/script/test.ps1
+++ 2001vue-total/script/test.ps1
@@ -2,3 +2,5 @@
 cd D:\develop\nginx-1.22.1
 # 查看nginx的版本
 .\nginx -v
+# 测试配置文件是否没有语法错误
+.\nginx -t -c .\conf\nginx.conf
